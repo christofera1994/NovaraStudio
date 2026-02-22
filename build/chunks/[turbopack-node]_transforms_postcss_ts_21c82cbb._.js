c01d56a67399959e55f7cf9f68685e4b2e708250
christofera1994 <christofera1994@gmail.com>
2026-02-22 20:58:23 +0100

    Add files via upload

---
+++ build/chunks/[turbopack-node]_transforms_postcss_ts_21c82cbb._.js
@@ -0,0 +1,13 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Desktop/novara/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "build/chunks/98808_ebb7b5f2._.js",
+  "build/chunks/[root-of-the-server]__4e34ee00._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/Desktop/novara/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
