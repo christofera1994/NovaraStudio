3847e25796e0ebdcf3b6a4c9e48eb8e6d28c4a73
christofera1994 <christofera1994@gmail.com>
2026-02-22 21:06:55 +0100

    Add files via upload

---
+++ api/contact/route.js
@@ -0,0 +1,8 @@
+var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/contact/route.js")
+R.c("server/chunks/98808_next_4d8f9b6e._.js")
+R.c("server/chunks/98808_@supabase_auth-js_dist_module_f3431fe2._.js")
+R.c("server/chunks/98808_5588a9b1._.js")
+R.c("server/chunks/[root-of-the-server]__9be505cb._.js")
+R.m("[project]/Desktop/novara/.next-internal/server/app/api/contact/route/actions.js [app-rsc] (server actions loader, ecmascript)")
+R.m("[project]/Desktop/novara/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/Desktop/novara/app/api/contact/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
+module.exports=R.m("[project]/Desktop/novara/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/Desktop/novara/app/api/contact/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
